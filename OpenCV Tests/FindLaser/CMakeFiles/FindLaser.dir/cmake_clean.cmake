5cfff5a884a19ebfae26c833f47296d80234a6d9
dheideman <dheideman@socal.rr.com>
2017-04-14 10:21:26 -0700

    initial commit

---
+++ OpenCV Tests/FindLaser/CMakeFiles/FindLaser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FindLaser.dir/FindLaser.cpp.o"
+  "FindLaser.pdb"
+  "FindLaser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FindLaser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
