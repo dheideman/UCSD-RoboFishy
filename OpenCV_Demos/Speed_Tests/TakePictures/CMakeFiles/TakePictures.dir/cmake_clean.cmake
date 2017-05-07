a2461c497e500965c107eec918e6d86090510152
dheideman <dheideman@socal.rr.com>
2017-05-06 18:54:32 -0700

    renamed folder

---
+++ OpenCV_Demos/Speed_Tests/TakePictures/CMakeFiles/TakePictures.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TakePictures.dir/TakePictures.cpp.o"
+  "TakePictures.pdb"
+  "TakePictures"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TakePictures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
