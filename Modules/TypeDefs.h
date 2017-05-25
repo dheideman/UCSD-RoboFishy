3ad3157d93d019e277442475db7ad313ea025cfa
dheideman <dheideman@socal.rr.com>
2017-05-24 23:16:00 -0700

    Revising paths in headers

--- Modules/TypeDefs.h
+++ Modules/TypeDefs.h
@@ -1,33 +1,12 @@
 /*****************************************************************************
- * PoCRangefinder.h
+ * TypeDefs.h
  *
- * Take images at varying distances from target to calibrate laser stick
+ * Define global submersible types
  * 
  ******************************************************************************/
 
-// OpenCV
-#include <cv.hpp>
-#include <highgui.h>
-#include "opencv2/imgproc/imgproc.hpp"
-
-// Streams
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <ostream>
-#include <string>
-
-// Global typedefs
-#include "../../Modules/TypeDefs.h"
-
-// Multithreading
-#include <pthread.h>
-#include <sched.h>
-#include <unistd.h>
-
-// V4L2
 #include <linux/videodev2.h>
-#include "../../Modules/V4L2Control/V4L2Control.h"
+#include "V4L2Control.h"
 
 //////////////////////
 // Type definitions //
@@ -61,16 +40,3 @@ typedef struct sub_state_t
   armed_t     laserarmed;     // Whether the laser can be turned on or not
 //   cv::Mat     imuorientation; // Orientation as determined by IMU
 } sub_state_t;
-
-/////////////////////////
-// Function Prototypes //
-/////////////////////////
-
-// Callback Functions
-void whiteBalanceCallback(int, void*);
-void mouseCallback(int event, int x, int y, int flags, void* userdata);
-void bcsCallback(int, void*);
-void exposureCallback(int, void*);
-
-// Threads
-void *takePictures(void*);
