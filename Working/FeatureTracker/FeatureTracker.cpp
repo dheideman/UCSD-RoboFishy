aa5f71ebeb673309e232ce4ff5bf73fee58df041
dheideman <dheideman@socal.rr.com>
2017-05-24 17:20:20 -0700

    Renamed variables, converted descriptors for use w/ FLANN

--- Working/FeatureTracker/FeatureTracker.cpp
+++ Working/FeatureTracker/FeatureTracker.cpp
@@ -32,7 +32,7 @@ int main(int argc, char** argv)
   VideoCapture cap(atoi(argv[1]));
   
     // Set video capture settings:
-///*
+/*
   // Get Codec Type- Int form
   int ex = static_cast<int>(cap.get(CV_CAP_PROP_FOURCC));
   
@@ -54,17 +54,17 @@ int main(int argc, char** argv)
   double y = 0;
   
   // Initialize frames
-  Mat img_object, img_scene;
+  Mat newimg, oldimg;
   
   for(int i=0; i<10; i++)
   {
-    cap.read(img_scene);
+    cap.read(oldimg);
   }
   
   while (waitKey(1) != 27)
   {
     // Get pictures
-    cap.read(img_object);
+    cap.read(newimg);
     /* Step 1: Detect the keypoints using Fast Detector */
     
     // Create detector
@@ -72,15 +72,15 @@ int main(int argc, char** argv)
     Ptr<FeatureDetector> detector = FastFeatureDetector::create();
 
     // Create vector of KeyPoints for each image 
-    vector<KeyPoint> keypoints_object, keypoints_scene;
+    vector<KeyPoint> newkeypoints, oldkeypoints;
 
     // Detect features (KeyPoints) in each image
-    detector->detect(img_object, keypoints_object);
-    detector->detect(img_scene, keypoints_scene);
+    detector->detect(newimg, newkeypoints);
+    detector->detect(oldimg, oldkeypoints);
     
     // Print out how many KeyPoints were detected in each image (DEBUG)
-//     cout << "# KeyPoints, object: " << keypoints_object.size() << endl;
-//     cout << "# KeyPoints, scene:  " << keypoints_scene.size() << endl;
+//     cout << "# KeyPoints, object: " << newkeypoints.size() << endl;
+//     cout << "# KeyPoints, scene:  " << oldkeypoints.size() << endl;
     
     
     /* Step 2: Calculate descriptors (feature vectors) */
@@ -92,57 +92,68 @@ int main(int argc, char** argv)
     // Create 2D matrix for descriptors for each image
     // Each row corresponds to a feature
     // 32 columns describe each feature
-    Mat descriptors_object, descriptors_scene;
+    Mat newdescriptors, olddescriptors;
         
     // Characterize each feature, store characteristics in Mats
-    extractor->compute(img_object, keypoints_object, descriptors_object);
-    extractor->compute(img_scene, keypoints_scene, descriptors_scene);
+    extractor->compute(newimg, newkeypoints, newdescriptors);
+    extractor->compute(oldimg, oldkeypoints, olddescriptors);
     
     // Print number of rows, columns for each image (DEBUG)
-//     cout << "# Rows, object: " << descriptors_object.rows << endl;
-//     cout << "# Cols, object: " << descriptors_object.cols << endl;
-//     cout << "# Rows, scene:  " << descriptors_scene.rows << endl;
-//     cout << "# Cols, scene:  " << descriptors_scene.cols << endl;
+//     cout << "# Rows, object: " << newdescriptors.rows << endl;
+//     cout << "# Cols, object: " << newdescriptors.cols << endl;
+//     cout << "# Rows, scene:  " << olddescriptors.rows << endl;
+//     cout << "# Cols, scene:  " << olddescriptors.cols << endl;
     
     // Make sure we have some descriptors, otherwise return error.
-    if ( descriptors_object.empty() )
+    if ( newdescriptors.empty() )
     {
-      cerr << "Error: descriptors_object is empty" << endl;
+      cerr << "Error: newdescriptors is empty" << endl;
       return -1;
     }
-    if ( descriptors_scene.empty() )
+    if ( olddescriptors.empty() )
     {
-      cerr << "Error: descriptors_scene is empty" << endl;
+      cerr << "Error: olddescriptors is empty" << endl;
       return -1;
+    }
+    
+    // Convert descriptors to CV_32F type for FLANN
+    if(newdescriptors.type()!=CV_32F)
+    {
+        newdescriptors.convertTo(newdescriptors, CV_32F);
+    }
+
+    if(olddescriptors.type()!=CV_32F)
+    {
+        olddescriptors.convertTo(olddescriptors, CV_32F);
     }
 
     /* Step 3: Matching descriptor vectors using FLANN matcher */
     
     // Create matcher
-//     Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("FlannBased");
+    // FLANN
+    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("FlannBased");
     // Brute Force:
 //     Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce");
-    Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming");
     
     // Create vector of matches
     vector< DMatch > matches;
     
     // Calculate matches between two images
-    matcher->match(descriptors_object, descriptors_scene, matches);
+    matcher->match(newdescriptors, olddescriptors, matches);
     
     // Write number of matches to screen (DEBUG)
 //     cout << "matches.size() = " << matches.size() << endl;
 
     // Calculate max and min distances between matched points
     double max_dist = 0; double min_dist = 200; double avg_dist = 0;
-    for (int i = 0; i < descriptors_object.rows; i++)
+    for (int i = 0; i < newdescriptors.rows; i++)
     {
         double dist = matches[i].distance;
         avg_dist += dist;
         if (dist < min_dist) min_dist = dist;
         if (dist > max_dist) max_dist = dist;
     }
-    avg_dist = avg_dist/descriptors_object.rows;
+    avg_dist = avg_dist/newdescriptors.rows;
     
     // Write min and max distances to screen
 //     printf("-- Max dist : %f \n", max_dist);
@@ -152,71 +163,71 @@ int main(int argc, char** argv)
     /* Draw only "good" matches (i.e. whose distance is less than 3*min_dist) */
     
     // Create vector of "good" matches
-    vector< DMatch > good_matches;
+    vector< DMatch > goodmatches;
     
     // Iterate through matches?
-//     for (int i = 0; i < descriptors_object.rows; i++)
+//     for (int i = 0; i < newdescriptors.rows; i++)
     for (int i = 0; i < matches.size(); i++)
     {
         if (matches[i].distance <= /*avg_dist/2*/ 10 * (min_dist+1))
         {
-            good_matches.push_back(matches[i]);
+            goodmatches.push_back(matches[i]);
         }
     }
     
     // Write number of "good" matches to screen (DEBUG)
-    cout << "good_matches.size() = " << good_matches.size() << endl;
+    cout << "goodmatches.size() = " << goodmatches.size() << endl;
     
-    if ( good_matches.size() == 0 )
+    if ( goodmatches.size() == 0 )
     {
       cerr << "Error: no good matches detected" << endl;
       return -1;
     }
     
     // Create matrix to store final output image
-    Mat img_matches;
+    Mat matchesimg;
     
     // Draw all those matches
-    drawMatches(img_object, keypoints_object, img_scene, keypoints_scene, good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+    drawMatches(newimg, newkeypoints, oldimg, oldkeypoints, goodmatches, matchesimg, Scalar::all(-1), Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
     /* Localize the object */
     
     // Create vectors of all the coordinates of the "good" matches
-    vector<Point2f> obj;
-    vector<Point2f> scene;
+    vector<Point2f> newpoints;
+    vector<Point2f> oldpoints;
 
-    for (int i = 0; i < good_matches.size(); i++)
+    for (int i = 0; i < goodmatches.size(); i++)
     {
         // Get the keypoints from the good matches
-        obj.push_back(keypoints_object[good_matches[i].queryIdx].pt);
-        scene.push_back(keypoints_scene[good_matches[i].trainIdx].pt);
+        newpoints.push_back(newkeypoints[goodmatches[i].queryIdx].pt);
+        oldpoints.push_back(oldkeypoints[goodmatches[i].trainIdx].pt);
     }
     
     // Write sizes of the localization vectors
-//     cout << "obj.size() = " << obj.size() << endl;
-//     cout << "scene.size() = " << scene.size() << endl;
+//     cout << "newpoints.size() = " << newpoints.size() << endl;
+//     cout << "oldpoints.size() = " << oldpoints.size() << endl;
      
     // Find perspective transformation between two planes.
-    Mat H = findHomography(obj, scene, CV_RANSAC);
+    Mat H = findHomography(newpoints, oldpoints, CV_RANSAC);
 
     // Create point arrays for image corners
-    vector<Point2f> obj_corners(4);
-    vector<Point2f> scene_corners(4);
+    vector<Point2f> newcorners(4);
+    vector<Point2f> oldcorners(4);
     
     // Create array of corner points of object image
-    obj_corners[0] = cvPoint(0, 0);
-    obj_corners[1] = cvPoint(img_object.cols, 0);
-    obj_corners[2] = cvPoint(img_object.cols, img_object.rows);
-    obj_corners[3] = cvPoint(0, img_object.rows);
+    newcorners[0] = cvPoint(0, 0);
+    newcorners[1] = cvPoint(newimg.cols, 0);
+    newcorners[2] = cvPoint(newimg.cols, newimg.rows);
+    newcorners[3] = cvPoint(0, newimg.rows);
     
     // Transform corner points by H to get corresponding points on scene image
-    perspectiveTransform(obj_corners, scene_corners, H);
+    perspectiveTransform(newcorners, oldcorners, H);
 
     // Draw box around detected object in scene image
-    line(img_matches, scene_corners[0] + Point2f(img_object.cols, 0), scene_corners[1] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
-    line(img_matches, scene_corners[1] + Point2f(img_object.cols, 0), scene_corners[2] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
-    line(img_matches, scene_corners[2] + Point2f(img_object.cols, 0), scene_corners[3] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
-    line(img_matches, scene_corners[3] + Point2f(img_object.cols, 0), scene_corners[0] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);  
+    line(matchesimg, oldcorners[0] + Point2f(newimg.cols, 0), oldcorners[1] + Point2f(newimg.cols, 0), Scalar(0, 255, 0), 4);
+    line(matchesimg, oldcorners[1] + Point2f(newimg.cols, 0), oldcorners[2] + Point2f(newimg.cols, 0), Scalar(0, 255, 0), 4);
+    line(matchesimg, oldcorners[2] + Point2f(newimg.cols, 0), oldcorners[3] + Point2f(newimg.cols, 0), Scalar(0, 255, 0), 4);
+    line(matchesimg, oldcorners[3] + Point2f(newimg.cols, 0), oldcorners[0] + Point2f(newimg.cols, 0), Scalar(0, 255, 0), 4);  
     
     // Write H to screen
     cout << "H = "<< endl << " "  << H << endl << endl;
@@ -237,14 +248,14 @@ int main(int argc, char** argv)
 //     cout << "Y: " << H.at<double>(1, 2) << endl;
     
     // Show detected matches
-    imshow("Good Matches & Object detection", img_matches);
+    imshow("Good Matches & Object detection", matchesimg);
     
     // Write frame to video
-    outputvideo.write(img_matches);
+//     outputvideo.write(matchesimg);
     
     // Copy newframe to oldframe.
-    img_object.copyTo(img_scene);
-//     imwrite("./SendaiTasselTest1b.jpg",img_matches);
+    newimg.copyTo(oldimg);
+//     imwrite("./SendaiTasselTest1b.jpg",matchesimg);
 //     waitKey(0);
   }
   return 0;
