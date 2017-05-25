38bca9f8f10121b6256e145cea36a24a0f2346c0
dheideman <dheideman@socal.rr.com>
2017-05-24 22:33:10 -0700

    Refactoring modules

--- Working/PoCRangefinder/PoCRangefinder.h
+++ Working/PoCRangefinder/PoCRangefinder.h
@@ -5,22 +5,29 @@
  * 
  ******************************************************************************/
 
+// OpenCV
 #include <cv.hpp>
 #include <highgui.h>
 #include "opencv2/imgproc/imgproc.hpp"
+
+// Streams
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <ostream>
 #include <string>
 
+// Global typedefs
+#include "../../../Modules/TypeDefs.h"
+
 // Multithreading
 #include <pthread.h>
 #include <sched.h>
 #include <unistd.h>
 
+// V4L2
 #include <linux/videodev2.h>
-#include "V4L2Control.h"
+#include "../../../Modules/V4L2Control/V4L2Control.h"
 
 //////////////////////
 // Type definitions //
