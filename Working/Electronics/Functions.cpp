60b0a56f2a24c44e29df05dd569d93afa8d100cf
sonagit <supadupastu@gmail.com>
2017-06-03 21:57:28 -0700

    removed and uses of system

--- Working/Electronics/Functions.cpp
+++ Working/Electronics/Functions.cpp
@@ -24,9 +24,9 @@ int initialize_motors(int channels[4], float freq)
 			for( i = 0; i < 3; i++ )
 			{
 				//pwmWrite (PIN_BASE+i, calcTicks(0,HERTZ));
-				pwmWrite (PIN_BASE+i, 2674);	//send input signal that is low enough to reach the 
+				pwmWrite (PIN_BASE+i, 2674);	//send input signal that is low enough to reach the
 												//"neutral" or power-off area in order to arm the ESC (long beep); green LED on ESC will light up
-				delay(1000);								
+				delay(1000);
 				active=0;
 			}
 		}
@@ -43,7 +43,7 @@ int saturate_number(float* val, float min, float max)
 		return 1;
 	}
 	else if(*val<min)
-	{	
+	{
 		*val = min;
 		return 1;
 	}
@@ -53,7 +53,7 @@ int saturate_number(float* val, float min, float max)
 
 /***************************************************************************
  * calib_t init_ms5837
- * 
+ *
  * Pressure Sensor Initialization
 ***************************************************************************/
 
@@ -70,13 +70,13 @@ calib_t init_ms5837(void)
 	pModule = PyImport_Import(pName);
 	pDict = PyModule_GetDict(pModule);
 	pFunc = PyDict_GetItemString(pDict, "init_press");
-	if (PyCallable_Check(pFunc)) 
+	if (PyCallable_Check(pFunc))
 	{
 		pValue = PyObject_CallObject(pFunc, NULL);
 		PyArg_ParseTuple(pValue,"ffffff", &calib.C1, &calib.C2, &calib.C3,&calib.C4, &calib.C5, &calib.C6);
 		Py_DECREF(pValue);
 	}
-	else 
+	else
 	{
 		PyErr_Print();
 	}
@@ -89,36 +89,36 @@ calib_t init_ms5837(void)
 ms5837_t ms5837_read(calib_t arg_in)	//	read pressure values from MS5837 pressure sensor
 {
 	ms5837_t ms5837;
-	
+
 	Py_Initialize();
-	
+
 	calib_t calib = arg_in; // create struct to hold calibration data
 	PyObject *pName, *pModule, *pDict, *pFunc, *pArgs, *pValue;
-	
+
 	pName = PyString_FromString("MS5837_example"); // input name of python source file
 	PyRun_SimpleString("import sys");
 	PyRun_SimpleString("sys.path.append(\"/home/pi/UCSD-RoboFishy/Working/Electronics/MainScript\")");
 	pModule = PyImport_Import(pName);
 	pDict = PyModule_GetDict(pModule);
 	pFunc = PyDict_GetItemString(pDict, "read_press");
-	
-	if (PyCallable_Check(pFunc)) 
+
+	if (PyCallable_Check(pFunc))
 	{
 
 		pArgs = Py_BuildValue("ffffff", calib.C1,calib.C2,calib.C3,calib.C4,calib.C5,calib.C6);
-		
+
 		pValue = PyObject_CallObject(pFunc, pArgs);
-		
+
 		//PyArg_ParseTuple(pValue,"ff", &ms5837.temperature, &ms5837.pressure);
 		PyArg_ParseTuple(pValue,"ff", &ms5837.pressure);
 		Py_DECREF(pArgs);
 		Py_DECREF(pValue);
 	}
-	else 
+	else
 	{
 		PyErr_Print();
 	}
-	
+
 	Py_DECREF(pModule);
 	Py_DECREF(pName);
 	Py_Finalize();
@@ -127,10 +127,9 @@ ms5837_t ms5837_read(calib_t arg_in)	//	read pressure values from MS5837 pressur
 
 void start_Py_ms5837(void)
 {
-	char cmd[50];
-    strcpy(cmd,"python MS5837_example.py & exit");
-    system(cmd);
-    return;
+	FILE* fd = fopen("python MS5837_example.py", "r");
+	PyRun_SimpleFile(fd,"python MS5837_example.py");
+  return;
 }
 
 
@@ -140,17 +139,13 @@ void start_Py_ms5837(void)
 
 void start_Py_bno055(void)	//	start bno055_read.py code
 {
-	
-//    char cmd[50];
-//    strcpy(cmd,"python bno055_read.py & exit");
     FILE* fd = fopen("bno055_read.py", "r");
     PyRun_SimpleFile(fd,"bno055_read.py");
-//    system(cmd);
     return;
 }
 
 bno055_t bno055_read(void)	// read values from bno055 IMU
-{ 
+{
 	bno055_t bno055;
 	char buf[1000];
 	FILE *fd = fopen( "bno055_fifo.txt", "r");
@@ -158,7 +153,7 @@ bno055_t bno055_read(void)	// read values from bno055 IMU
 
 	fgets(buf,1000,fd);
 	fclose(fd);
-	sscanf(buf,"%f %f %f %f %f %f %i %i %i %i", 
+	sscanf(buf,"%f %f %f %f %f %f %i %i %i %i",
 	&bno055.yaw,&bno055.roll,&bno055.pitch,
 	&bno055.q, &bno055.p, &bno055.r,
 	&bno055.sys,&bno055.gyro,&bno055.accel,&bno055.mag);
@@ -171,9 +166,8 @@ bno055_t bno055_read(void)	// read values from bno055 IMU
 
 void start_Py_ds18b20(void)		//	start temperature_sensor_code.py code
 {
-	char cmd[50];
-	strcpy(cmd,"python temperature_sensor_code.py & exit");
-	system(cmd);
+	FILE* fd = fopen("python temperature_sensor_code.py", "r");
+	PyRun_SimpleFile(fd,"python temperature_sensor_code.py");
 	return;
 }
 
@@ -192,14 +186,14 @@ ds18b20_t ds18b20_read(void)	// read values from ds18b20 temperature sensor
 ///////////////////////////////Setup and Shutdown//////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 
-// Initialization Script: This script runs all of the initialization functions // 
+// Initialization Script: This script runs all of the initialization functions //
 int scripps_auv_init(void)
 {
 	start_Py_bno055();			// start IMU
-	sleep(10);			
+	sleep(10);
 	//start_Py_ms5837();		// start pressure sensor
 	start_Py_ds18b20();			// start temperature sensor
-	signal(SIGINT, ctrl_c);	
+	signal(SIGINT, ctrl_c);
 	return 0;
 }
 
