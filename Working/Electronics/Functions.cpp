4036dc0191b80597bbd66162cf4f305980045ada
gmarcy07 <gmarcy07@gmail.com>
2017-06-04 17:12:55 -0700

    Fixed tabs

--- Working/Electronics/Functions.cpp
+++ Working/Electronics/Functions.cpp
@@ -324,29 +324,29 @@ int cleanup_auv()
 int yaw_controller()
 {
 // read IMU values into sstate
-		bno055 = bno055_read();
+	bno055 = bno055_read();
 
-		// control output //
-					if(bno055.yaw<180) // AUV is pointed right
-					{
-						// u[2] is negative
-						u[2] = KP_YAW*(bno055.yaw-sstate.yaw[0]); //+ KD_YAW*(sstate.yaw[0]-sstate.yaw[1])/DT; // yaw controller
-					}
-					else		// AUV is pointed left
-					{
-						// u[2] is positive
-						u[2] = KP_YAW*(setpoint.yaw-(sstate.yaw[0]-360)); //+ KD_YAW*(sstate.yaw[0]-sstate.yaw[1])/DT; // yaw controller
-					}
+	// control output //
+	if(bno055.yaw<180) // AUV is pointed right
+	{
+		// u[2] is negative
+		motor_percent = KP_YAW*(bno055.yaw-sstate.yaw[0]); //+ KD_YAW*(sstate.yaw[0]-sstate.yaw[1])/DT; // yaw controller
+	}
+	else		// AUV is pointed left
+	{
+		// u[2] is positive
+		motor_percent = KP_YAW*(setpoint.yaw-(sstate.yaw[0]-360)); //+ KD_YAW*(sstate.yaw[0]-sstate.yaw[1])/DT; // yaw controller
+	}
 
-					// saturate yaw controller //
-					if(u[2]>YAW_SAT)
-					{
-						u[2]=YAW_SAT;
-					}
-					else if(u[2]<-YAW_SAT)
-					{
-						u[2]=-YAW_SAT;
-					}
+	// saturate yaw controller //
+	if(motor_percent>YAW_SAT)
+	{
+		u[2]=YAW_SAT;
+	}
+	else if(u[2]<-YAW_SAT)
+	{
+		u[2]=-YAW_SAT;
+	}
 		
 }
 /***************************************************************************
