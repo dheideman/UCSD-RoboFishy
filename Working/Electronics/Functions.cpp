3b2565a8d6d8dcf8bf3595866e46b40d8da03ea9
gmarcy07 <gmarcy07@gmail.com>
2017-06-04 20:40:11 -0700

    Created DS18b20.cpp

--- Working/Electronics/Functions.cpp
+++ Working/Electronics/Functions.cpp
@@ -62,46 +62,6 @@ int saturate_number(float* val, float min, float max)
 
 
 
-
-///////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////// DS18B20 Functions ////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////
-
-/***************************************************************************
- * void start_Py_ds18b20
- *
- * Writes temperature values from the DS18B20 temperature sensor into
- * ds18b20_fifo.fifo
-***************************************************************************/
-
-void start_Py_ds18b20(void)		//	start temperature_sensor_code.py code
-{
-	FILE* fd = fopen("python temperature_sensor_code.py", "r");
-	PyRun_SimpleFile(fd,"python temperature_sensor_code.py");
-	return;
-}
-
-/***************************************************************************
- * ds18b20_t ds18b20_read(void)
- *
- * Reads temperature values from ds18b20_fifo.fifo
-***************************************************************************/
-
-ds18b20_t ds18b20_read(void)	// read values from ds18b20 temperature sensor
-{
-	ds18b20_t ds18b20;
-	char buf[1000];
-	FILE *fd = fopen( "/home/pi/UCSD-RoboFishy/Working/Electronics/ds18b20_fifo.fifo", "r");
-	fgets(buf,1000,fd);
-	fclose(fd);
-	sscanf(buf,"%f",&ds18b20.temperature);
-	return ds18b20;
-}
-
-///////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////Setup and Shutdown//////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////
-
 /***************************************************************************
  * int scripps_auv_init(void)
  *
