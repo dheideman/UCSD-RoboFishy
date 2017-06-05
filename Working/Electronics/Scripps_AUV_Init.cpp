132032b47f7e28027b5fe5bfd275e0c1733659a0
j2ho <justinho515@gmail.com>
2017-06-05 14:57:05 -0700

    asdf

--- Working/Electronics/Scripps_AUV_Init.cpp
+++ Working/Electronics/Scripps_AUV_Init.cpp
@@ -13,17 +13,10 @@
 
 int scripps_auv_init(void)
 {
-<<<<<<< HEAD
-	start_Py_bno055();				// start IMU Python script
-	sleep(10);
-	//start_Py_ms5837();			// start pressure sensor
-	start_Py_ds18b20();				// start temperature sensor
-=======
 	start_Py_bno055();			// start IMU
 	sleep(1);
 	//start_Py_ms5837();		// start pressure sensor
 	start_Py_ds18b20();			// start temperature sensor
->>>>>>> c35849547d66cc742094ed2eec39df1b9c941c14
 	signal(SIGINT, ctrl_c);		// capture ctrl+c and exit
 	return 0;
 }
