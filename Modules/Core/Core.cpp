bca3c584b38be49afa93c09df5271bc025bdf37c
Jacob Harvey <jtharvey@github.com>
2017-06-08 16:34:02 -0700

    moved ctrl-c handler

--- Modules/Core/Core.cpp
+++ Modules/Core/Core.cpp
@@ -70,3 +70,17 @@ void destroyTAttr()
   
   return;
 }
+
+/***************************************************************************
+ * void ctrl_c(int signo)
+ *
+ * Captures a user-entered ctrl+c and exits the script
+***************************************************************************/
+void ctrl_c(int signo)
+{
+  if (signo == SIGINT)
+  {
+    substate.mode = STOPPED;
+    printf("\nReceived SIGINT Ctrl-C\n");
+  }
+}
