5b368cba672bd09c0c68d9c3f632338e6ca80e4a
sonagit <supadupastu@gmail.com>
2017-06-09 12:01:20 -0700

    init works, now to seg fault...again

--- Working/Electronics/IMU_Functions.cpp
+++ Working/Electronics/IMU_Functions.cpp
@@ -20,7 +20,7 @@ void start_read_imu(void)
     // clear fifo file //
     std::FILE* fd = fopen("imu.fifo","w");
     fclose(fd);
-    printf("Created and cleared imu.fifo\n");
+    printf("\nCreated and cleared imu.fifo\n");
 
     printf("Starting read_imu.py, then waiting 2 seconds\n");
     char cmd[50];
@@ -34,7 +34,7 @@ void start_read_imu(void)
     std::ifstream pFile("imu.fifo");
     if(pFile.peek() == std::ifstream::traits_type::eof())
     {
-      printf("read_imu.py has NOT written to imu.fifo\n");
+      printf("\nERROR: -----> read_imu.py has NOT written to imu.fifo\n");
       printf("Retrying IMU initialization\n");
       // if it isn't printing values, restart initialization
     }
