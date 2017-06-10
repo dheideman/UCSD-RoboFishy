4271b997e7e6d45429136cd71d03e79398a00342
sonagit <supadupastu@gmail.com>
2017-06-09 18:25:06 -0700

    function consolidation

---
+++ Mapper/Controller_Functions.cpp
@@ -0,0 +1,141 @@
+/******************************************************************************
+* Controls.cpp
+*
+* Contains the marchPID() and depth_controller functions
+******************************************************************************/
+#include "Mapper.h"
+
+/******************************************************************************
+* float marchPID()
+*
+* Takes in readings from the IMU and returns a value between -1 and 1 (-100% -
+* +100%) that the port and starboard thrusters should run at
+******************************************************************************/
+
+float marchPID(pid_data_t controller, float input)
+{
+	// Calculating errors
+	controller.perr = input - controller.setpoint;
+	controller.derr = (input - controller.old)/(controller.dt);
+	controller.ierr += controller.dt * (input - controller.setpoint);
+
+	if(controller.ierr > controller.isat)
+	{
+		controller.ierr = controller.isat;
+	}
+
+	if(controller.ierr < -controller.isat)
+	{
+		controller.ierr = - controller.isat;
+	}
+
+	controller.output =   controller.kp * controller.perr
+						+ controller.ki * controller.ierr
+						+ controller.kd * controller.derr;
+
+	if(controller.output > controller.sat)
+	{
+		controller.output = controller.sat;
+	}
+
+
+	if(controller.output < -controller.sat)
+	{
+		controller.output = -controller.sat;
+	}
+	// set current input to be the old input //
+	controller.old = input;
+
+	return controller.output;
+}
+
+
+/******************************************************************************
+ * int initialize_motors(int channels[3], float freq)
+ *
+ * Initializes the PWM board and the ESCs that run the motors
+******************************************************************************/
+int initialize_motors(int channels[3], float freq)
+{
+	// integer specifying motor number //
+	int i;
+
+	// setup PCA9685 PWM board //
+	int fd = pca9685Setup(PIN_BASE, PCA9685_ADDR, HERTZ);
+	if (fd < 0)
+	{
+		printf("Error in setup\n");
+		return fd;
+	}
+
+	// reset all PCA9685 PWM board output //
+	pca9685PWMReset(fd);
+
+	// set motor outputs to 0 to initialize ESCs //
+	int active=1;
+		while (active)
+		{
+			for( i = 0; i < 3; i++ )
+			{
+				// send "neutral" signal to arm ESCs //
+				pwmWrite (PIN_BASE+i, MOTOR_0);
+				active=0;
+			}
+		}
+	return fd;
+}
+
+/******************************************************************************
+ * int set_motor(int motornum, float percent)
+ *
+ * Takes in a value from -1 to 1 (-100 to +100%) and sets the motor
+ * outputs accordingly
+******************************************************************************/
+int set_motor(int motornum, float percent)
+{
+  // Define characteristics of PWM pulse, microseconds
+	float amplitude  = PWM_HIGH_LIMIT - PWM_ZERO_VALUE;
+
+	// Saturation limits
+  if( percent >  1.0) percent =  1.0;
+  if( percent < -1.0) percent = -1.0;
+
+  // Deadzone check
+  if( (percent < MOTOR_DEADZONE) && (percent > -MOTOR_DEADZONE) ) percent = 0.0;
+
+	// Calculate corresponding pwm output value
+	int motoroutput = percent * amplitude + PWM_ZERO_VALUE;
+
+	// Spin those motors
+  pwmWrite(motornum + PIN_BASE, motoroutput);
+
+  #ifdef DEBUG
+  // Print what we told the motor to spin at.
+  printf("Set motor %d to %d \n",motornum, motoroutput);
+  #endif
+
+	return 1;
+}
+
+/***************************************************************************
+ * int saturate_number(float* val, float min, float max)
+ *
+ * Generic function for saturating values in a function
+***************************************************************************/
+int saturate_number(float* val, float min, float max)
+{
+	// if "val" is greater than "max", set "val" to "max" //
+	if(*val>max)
+	{
+		*val = max;
+		return 1;
+	}
+
+	// if "val" is less than "min", set "val" to "min" //
+	else if(*val<min)
+	{
+		*val = min;
+		return 1;
+	}
+	return 0;
+}
---
+++ Mapper/Sensor_Functions.cpp
@@ -0,0 +1,201 @@
+/******************************************************************************
+ * initialize_sensors.cpp
+ *
+ * Contains the scripps_auv_init() function
+******************************************************************************/
+#include "Mapper.h"
+
+/******************************************************************************
+ * int scripps_auv_init(void)
+ *
+ * Initializes the IMU, pressure sensor, and temperature sensor
+******************************************************************************/
+
+int initialize_sensors(void)
+{
+	printf("Initializing sensors...\n");
+	start_read_imu();					// start IMU
+	printf("IMU data is being read\n\n");
+	auv_usleep(100000);
+
+	start_read_pressure();		// start pressure sensor
+	printf("Pressure is being read\n\n");
+	auv_usleep(100000);
+
+	start_read_temp();				// start temperature sensor
+	printf("Temp is being read\n\n");
+	auv_usleep(100000);
+
+	signal(SIGINT, ctrl_c);		// capture ctrl+c and exit
+	return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//															IMU FUNCTIONS
+///////////////////////////////////////////////////////////////////////////////
+/******************************************************************************
+ * void start_read_imu
+ *
+ * Starts read_imu.py code
+******************************************************************************/
+void start_read_imu(void)
+{
+  // Create flag for continuing
+  bool success = false;
+  while (!success)
+  {
+    // clear fifo file //
+    std::FILE* fd = fopen("imu.fifo","w");
+    fclose(fd);
+    printf("\nCreated and cleared imu.fifo\n");
+
+    printf("Starting read_imu.py, then waiting 3 seconds\n");
+    char cmd[100];
+    strcpy(cmd,"nohup python -u read_imu.py > read_imu.log 2>&1 < /dev/null & exit");
+    system(cmd);
+    printf("Started IMU using system(cmd)\n");
+    // Wait 2 seconds to let the python script start up
+    auv_usleep(3000000);
+
+    // Check whether the python script wrote anything
+    std::ifstream pFile("imu.fifo");
+    if(pFile.peek() == std::ifstream::traits_type::eof())
+    {
+      printf("\nERROR: -----> read_imu.py has NOT written to imu.fifo\n");
+      printf("Retrying IMU initialization\n");
+      // if it isn't printing values, restart initialization
+    }
+    else
+    {
+      printf("read_imu.py HAS written to imu.fifo\n");
+      // success! continue
+      printf("IMU Initialized\n");
+      success = true;
+    }
+  }
+  return;
+}
+
+/******************************************************************************
+ * imu_t read_imu_fifo
+ *
+ * Reads IMU values from imu.fifo and write to the imu struct
+******************************************************************************/
+imu_t read_imu_fifo(void)
+{
+	imu_t imu;
+	char buf[1000];
+	FILE *fd = fopen( "imu.fifo", "r");
+	fgets(buf,1000,fd);
+	fclose(fd);
+	sscanf(buf,"%f %f %f %f %f %f %i %i %i %i %f %f %f",
+				 &imu.yaw,  &imu.roll,  &imu.pitch,
+				 &imu.p,    &imu.q,     &imu.r,
+				 &imu.sys,  &imu.gyro,  &imu.accel,
+				 &imu.mag,  &imu.x_acc, &imu.y_acc, &imu.z_acc);
+
+	return imu;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//															Pressure FUNCTIONS
+///////////////////////////////////////////////////////////////////////////////
+
+/******************************************************************************
+ * void start_read_pressure
+ *
+ * Starts pressure reading python program
+******************************************************************************/
+void start_read_pressure(void)
+{
+  // Create flag for continuing
+  bool success = false;
+  while (!success)
+  {
+    // clear fifo file //
+    std::FILE* fd = fopen("pressure.fifo","w");
+    fclose(fd);
+    printf("\nCreated and cleared pressure.fifo\n");
+
+    printf("Starting read_pressure.py, then waiting 3 seconds\n");
+  	char cmd[50];
+    strcpy(cmd, "nohup python -u read_pressure.py > read_pressure.log 2>&1 < /dev/null & exit");
+    system(cmd);
+    printf("Started read_pressure.py using system(cmd)\n");
+    auv_usleep(3000000);
+
+    // Check whether the python script wrote anything
+    std::ifstream pFile("pressure.fifo");
+    if(pFile.peek() == std::ifstream::traits_type::eof())
+    {
+      printf("\nERROR: -----> read_pressure.py has NOT written to pressure.fifo\n");
+      printf("Retrying Pressure Sensor initialization\n");
+      // if it isn't printing values, restart initialization
+    }
+    else
+    {
+      printf("read_pressure.py HAS written to pressure.fifo\n");
+      // success! continue
+      printf("Pressure Sensor Initialized\n");
+      success = true;
+    }
+  }
+  return;
+}
+
+/******************************************************************************
+ * ms5837_t read_pressure_fifo
+ *
+ * Reads depth and temp values from pressure.fifo and write to the ms5837 struct
+******************************************************************************/
+ms5837_t read_pressure_fifo(void)
+{
+	ms5837_t ms5837;
+	char buf[1000];
+	FILE *fd = fopen( "pressure.fifo", "r");
+	fgets(buf, 1000, fd);
+	fclose(fd);
+	sscanf(buf, "%f %f", &ms5837.depth, &ms5837.water_temp);
+	return ms5837;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//															Temp FUNCTIONS
+///////////////////////////////////////////////////////////////////////////////
+
+/******************************************************************************
+ * void start_read_temp(void)
+ *
+ * Writes temperature values from the DS18B20 temperature sensor into
+ * temp.fifo
+******************************************************************************/
+void start_read_temp(void)
+{
+char cmd[100];
+  printf("\nStarting read_temp.py, then waiting 2 seconds\n");
+  strcpy(cmd, "nohup python -u read_temp.py > read_temp.log 2>&1 < /dev/null & exit");
+  system(cmd);
+  printf("Started read_temp.py using system(cmd)\n");
+  auv_usleep(2000000);
+  return;
+}
+
+/******************************************************************************
+ * float read_temp_fifo(void)
+ *
+ * Reads temperature values from temp.fifo
+******************************************************************************/
+float read_temp_fifo(void)
+{
+	float temp;
+
+	// Read temperature values from temp.fifo
+	char buf[100];
+  std::FILE *fd = fopen("temp.fifo", "r");
+	fgets(buf, 100, fd);
+	fclose(fd);
+	sscanf(buf, "%f", &temp);
+
+	// Return a temperature value
+	return temp;
+}
