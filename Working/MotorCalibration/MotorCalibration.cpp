886efaec2fbcf9e9651e76f705cdbe655e29e28c
sonagit <supadupastu@gmail.com>
2017-06-15 10:12:06 -0700

    MoCal changes

--- Working/MotorCalibration/MotorCalibration.cpp
+++ Working/MotorCalibration/MotorCalibration.cpp
@@ -14,7 +14,7 @@ int stoploop = 0;
 
 /******************************************************************************
 * void ctrlC(int signo)
-* 
+*
 * Control-C Handler
 ******************************************************************************/
 void ctrlC(int signo)
@@ -23,7 +23,7 @@ void ctrlC(int signo)
 	{
 		stoploop = 1;
 		printf("\nReceived SIGINT Ctrl-C\n");
-    
+
  	  for( int i = 0; i < 3; i++ )
   	{
 		  pwmWrite (300+i, 2674);
@@ -41,7 +41,7 @@ int main()
 {
 	// Initialize WiringPi
  	wiringPiSetupGpio();
-	
+
 	// Setup ctrl-c catcher
 	signal(SIGINT, ctrlC);
 
@@ -52,32 +52,55 @@ int main()
 		pwmWrite (PIN_BASE+i, 2674);
 		printf("Initialized motor at pin_base: %i\n", PIN_BASE+i);
 	}
-  
+
+	int motornum, motoroutput;
+
 	// Run main while loop, wait until it's time to stop
 	while(!stoploop)
 	{
-		int motornum = 0, motoroutput;
-	
-		//std::cout << "Input motor number: ";
-		//std::cin  >> motornum;
-	
+		std::cout << "Input motor number: ";
+		std::cin  >> motornum;
+
 		std::cout << "Input motor output: ";
 		std::cin  >> motoroutput;
-		
+
 		// Spin those motors
-		//pwmWrite(motornum + PIN_BASE, motoroutput);
-		pwmWrite(PIN_BASE + 0, motoroutput);
-		pwmWrite(PIN_BASE + 1, motoroutput);
-		pwmWrite(PIN_BASE + 2, motoroutput);
-		
+		motoroutput = set_motor(motornum, motoroutput);
+
 		std::cout << "Set Motor " << motornum << " to " << motoroutput << std::endl << std::endl;
 	}
-
-	// Shut off motors
-	//pwmWrite(PIN_BASE + 0, 2647);
-	//pwmWrite(PIN_BASE + 1, 2647);
-	//pwmWrite(PIN_BASE + 2, 2647);
 
 	return 0;
 }
 
+/******************************************************************************
+ * float set_motor(int motornum, float percent)
+ *
+ * Takes in a value from -1 to 1 (-100 to +100%) and sets the motor
+ * outputs accordingly
+******************************************************************************/
+float set_motor(int motornum, float percent)
+{
+	// Define characteristics of PWM pulse, microseconds
+	float amplitude	 = (PWM_HIGH_LIMIT - PWM_ZERO_VALUE)/100;
+
+	// Saturation limits
+	if( percent >	 100) percent =	 100;
+	if( percent < -100) percent = -100;
+
+	// Deadzone check
+	if( (percent < MOTOR_DEADZONE) && (percent > -MOTOR_DEADZONE) ) percent = 0.0;
+
+	// Calculate corresponding pwm output value
+	int motoroutput = percent * amplitude + PWM_ZERO_VALUE;
+
+	// Spin those motors
+	pwmWrite(motornum + PIN_BASE, motoroutput);
+
+	#ifdef DEBUG
+	// Print what we told the motor to spin at.
+	printf("Set motor %d to %d \n", motornum, motoroutput);
+	#endif
+
+	return percent;
+}
--- Working/MotorCalibration/MotorCalibration.h
+++ Working/MotorCalibration/MotorCalibration.h
@@ -19,6 +19,13 @@
 #define HERTZ 400
 #define PCA9685_ADDR 0x40
 
+// Motor Constants
+#define BASE_SPEED     10     // 10 percent
+#define MOTOR_DEADZONE 2      // 2 percent
+#define PWM_LOW_LIMIT  1940   // PWM value
+#define PWM_HIGH_LIMIT 3354   // PWM value
+#define PWM_ZERO_VALUE 2647   // PWM value
+
 // Core Module
 #include "../../Modules/Core/Core.h"
 
